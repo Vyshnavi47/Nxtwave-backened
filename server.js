fbc8739a4cbc40795828de5ca6ff8e44913cbf08
vyshnavi47 <vyshnavi.sappa@sasi.ac.in>
2025-05-05 18:54:27 +0530

    added new package cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDb = require("./config/dbConnection")
+const cors = require("cors");
 connectDb();
 const app=express();
+app.use(cors("*"));
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/users", require("./Routes/UserRoutes"))
