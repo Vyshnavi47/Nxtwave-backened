2c615775ed1c57bdfb48ae98babfb23325cb84ab
vyshnavi47 <vyshnavi.sappa@sasi.ac.in>
2025-05-05 20:26:13 +0530

    updated the index.js

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(cors({
 
 const port = process.env.PORT || 5000;
 app.use(express.json());
+app.get("/",(req,res)=>{
+    res.send("hello from the server")
+})
 app.use("/api/users", require("./Routes/UserRoutes"))
 app.listen(port,()=>{
     console.log(`Server running at ${port}`);
